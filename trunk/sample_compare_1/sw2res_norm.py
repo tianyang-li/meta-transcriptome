7a95f63debe2324d337a759e857ce3610776da15
tmy1018.1@gmail.com <tmy1018.1@gmail.com@2d419d04-3b34-27e7-1591-4006bd207e75>
2011-09-20 07:47:22 +0000

    code faster

---
+++ trunk/sample_compare_1/sw2res_norm.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+'''
+SW scores from the output of sw_2.py is normalized in the following way:
+    SW_score / align_len
+'''
+
+import sys
+
+if __name__ == '__main__':
+    sys.exit(0)
