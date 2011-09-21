8f521e9c4396f71c53419265c51ecf254a27c75b
tmy1018.1@gmail.com <tmy1018.1@gmail.com@2d419d04-3b34-27e7-1591-4006bd207e75>
2011-09-21 12:21:51 +0000

    use normalized score that appears in another paper

--- sample_compare_1/sw2res_norm.py
+++ sample_compare_1/sw2res_norm.py
@@ -6,6 +6,36 @@ SW scores from the output of sw_2.py is normalized in the following way:
 '''
 
 import sys
+import string
+
+def SWNorm(swres):
+    def StripWater(fname):
+        return ((fname[::-1]).replace("retaw.", "", 1))[::-1]
+        
+    fout = open(StripWater(swres) + ".norm", 'w')
+    fin = open(swres, 'r')
+    
+    while True:
+        line = fin.readline()
+        if line == "":
+            break
+        line = string.strip(line)
+        strip_len = line.replace("# Length: ", "", 1)
+        if line != strip_len:
+            align_len = float(strip_len)
+            fin.readline()  # identity
+            fin.readline()  # similarity
+            fin.readline()  # gaps
+            line = fin.readline()
+            line = string.strip(line)
+            line = line.replace("# Score: ", "", 1)
+            sw_score = float(line)
+            norm_s = sw_score / align_len
+            fout.write("%f\n" % norm_s)
+        
+    fin.close()
+    fout.close()
 
 if __name__ == '__main__':
+    SWNorm(sys.argv[1])
     sys.exit(0)
