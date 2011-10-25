86fbdb1b0723c51b72e52b3208d902940f8a6001
tmy1018.1@gmail.com <tmy1018.1@gmail.com@2d419d04-3b34-27e7-1591-4006bd207e75>
2011-10-25 15:24:09 +0000

    code faster.. get it done quicker.. maybe u can make it...

--- sample_compare_1/blast_info.py
+++ sample_compare_1/blast_info.py
@@ -9,5 +9,19 @@ blastrec = NCBIXML.parse(open(sys.argv[1]))
 
 for rec in blastrec:
     if len(rec.alignments) > 0:
+        print "*************************************** Query START"
+        print rec.query
+        print "*************************************** Query END"
         for (desc, al) in itertools.izip(rec.descriptions, rec.alignments):
+            print "Description"
+            print "title: %s" % desc.title
+            print "e: %lf" % desc.e
+            print "number of alignments: %d" % desc.num_alignments
+            print "Alignment"
+            print "title: %s" % al.title
+            print "HSPs:"
+            for hsp in al.hsps:
+                print "\t%s" % hsp.query
+                print "\t%s" % hsp.match
+                print "\t%s" % hsp.sbjct
             raw_input("################## Press a key to continue...............")
