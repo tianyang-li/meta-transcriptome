1e0903688b250bfbc9ba3fea44ccac200cee56ff
tmy1018.1@gmail.com <tmy1018.1@gmail.com@2d419d04-3b34-27e7-1591-4006bd207e75>
2011-11-26 14:10:14 +0000

    run this on real data:wq

--- microbe-trans-analysis/seq_kmer.py
+++ microbe-trans-analysis/seq_kmer.py
@@ -19,4 +19,76 @@
 
 """
 Analyze kmer frequencies and repeats in FASTA or FASTQ sequences
+
+Usage:
+./seq_kmer.py [read type: fasta/fastq] [kmer length] 
+    [read file 1 (fasta or fastq)] [read file 2 (fasta or fastq)] ...
 """
+
+from Bio import SeqIO
+import sys
+
+def SingleSeqKmer(read_str, k):
+    """
+    Kmer statistics for read_str
+    
+    Arguments:
+        read_str: a string of ACGT sequences
+        
+    Returns:
+        dictionary containing kmer frequencies
+    """
+    kmer_count = {}
+    
+    for kmer_s in range(len(read_str) - k + 1):
+        kmer = read_str[kmer_s : kmer_s + k - 1]
+        if kmer not in kmer_count:
+            kmer_count[kmer] = 1
+        else:
+            kmer_count[kmer] += 1
+        
+    return kmer_count
+
+def main(argv):
+    k = int(argv[2])
+    
+    # a list of [read, kmer_count (dict: (kmer, count))
+    #     , kmer_self_repeat (int), read_overlap_kmer (list: (read, overlap_kmer))]  
+    reads = []  
+    
+    for rf in argv[3:]:
+        for read in SeqIO.parse(rf, argv[1]):
+            reads.append([read])
+    
+    for read_entry in reads:
+        kmer_count = SingleSeqKmer(str(read_entry[0].seq), k)
+        read_entry.append(kmer_count)
+        
+        kmer_self_repeat = 0
+        for kmer, occur in kmer_count.iteritems():
+            if occur > 1:
+                kmer_self_repeat += 1
+        read_entry.append(kmer_self_repeat)
+        
+        read_overlap_kmer = []
+        read_entry.append(read_overlap_kmer)
+    
+    for ent1 in range(len(reads)):
+        for ent2 in range(ent1 + 1, len(reads)):
+            set1 = set(reads[ent1][1].keys())
+            set2 = set(reads[ent2][1].keys())
+            overlap_kmer = list(set1.intersection(set2))
+            if len(overlap_kmer) != 0:
+                reads[ent1][3].append((ent2, overlap_kmer))
+                reads[ent2][3].append((ent1, overlap_kmer))
+    
+    for read_entry in reads:
+        print read_entry[2], len(read_entry[3])
+            
+        
+if __name__ == '__main__':
+    main(sys.argv)
+    sys.exit(0)
+
+
+
