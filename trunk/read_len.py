2f41dfa342f6f106a7acbf4857d91d4351987cf1
tmy1018.1@gmail.com <tmy1018.1@gmail.com@2d419d04-3b34-27e7-1591-4006bd207e75>
2011-12-04 07:07:50 +0000

    do things faster

--- trunk/read_len.py
+++ trunk/read_len.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python
 
+#  Copyright (C) 2011 Tianyang Li
+#
+#  tmy1018 (at) gmail (dot) com
+#
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+
+"""
+./read_len.py [fasta/fastq] [read files]
+"""
+
 import sys
 from Bio import SeqIO
 
 def main(argv):
+    for fin in argv[2:]:
+        for read in SeqIO.parse(fin, argv[1]):
+            print len(str(read.seq))
 
 if __name__ == '__main__':
     main(sys.argv)
