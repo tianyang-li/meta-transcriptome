3d50eccfe24e028dbfb6534c8c0e5ca40dcd8417
tmy1018.1@gmail.com <tmy1018.1@gmail.com@2d419d04-3b34-27e7-1591-4006bd207e75>
2012-01-30 04:47:44 +0000

    bioperl working now?

---
+++ trunk/random-coverage/sim-single.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+#  Copyright (C) 2011 Tianyang Li
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+
+# simluate uniform random covering of a 
+# transcript by reads of equal length
+# 
+# this is a model for Illumina single end reads
+
+use Bio::SeqIO;
+
