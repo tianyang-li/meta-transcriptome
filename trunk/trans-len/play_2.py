70273553b1c8f8c32afdf467a344de2dd3f51d4f
tmy1018.1@gmail.com <tmy1018.1@gmail.com@2d419d04-3b34-27e7-1591-4006bd207e75>
2012-02-25 08:30:54 +0000

    code faster

--- trunk/trans-len/play_2.py
+++ trunk/trans-len/play_2.py
@@ -14,10 +14,45 @@
 #
 # You should have received a copy of the GNU General Public License
 
+"""
+(effective) single contig length distribution 
+given (effective) length $L$, (effective) read 
+length $k$, and the number of reads $N$
+"""
+
 import getopt
 import sys
 
 from nzmath.combinatorial import stirling1
 from scipy import comb
+
+import matplotlib.pyplot as plt
+
+def single_contig_len_prb(L, N, k):
+    prob = []
+    #TODO
+    sum = 0
+    for x in prob:
+        sum += x
+    map(lambda x: x / sum, prob)
+    return prob
+
+def main(args):
+    L, N, k = None, None, None
+    try:
+        opts, args = getopt.getopt(args, 'L:N:k:')
+    except getopt.GetoptError as err:
+        print >> sys.stderr, str(err)
+        sys.exit(2)
+    if L == None or N == None or k == None:
+        print >> sys.stderr, "Missing options"
+        sys.exit(2)
+    prob = single_contig_len_prb(L, N, k)
+    plt.plot(range(len(prob)),prob)
+    plt.grid(True)
+    plt.show()
+    
+if __name__ == '__main__':
+    main(sys.argv[1:])
 
 
--- trunk/trans-len/play_3.py
+++ trunk/trans-len/play_3.py
@@ -15,35 +15,46 @@
 # You should have received a copy of the GNU General Public License
 
 """
-$\binom{n}{m} \binom{r - 1 - mk}{n - 1}$
+(effective) single contig length distribution 
+given $N$ reads and (effective) read length $k$
 """
 
 import getopt
 import sys
+
+from nzmath.combinatorial import stirling1
 from scipy import comb
 
+import matplotlib.pyplot as plt
+
+def rand_read_len_prob(N, k):
+    prob = []
+    #TODO
+    sum = 0
+    for x in prob:
+        sum += prob
+    map(lambda x: x / sum, prob)
+    return prob
+
 def main(args):
-    n, r, k = None, None, None
+    N, k = None, None
     try:
-        opts, args = getopt.getopt(args, 'n:r:k:')
+        opts, args = getopt.getopt(args, 'N:k:')
     except getopt.GetoptError as err:
         print >> sys.stderr, str(err)
-        sys.exit(2) 
+        sys.exit(2)
     for o, a in opts:
-        if o == '-n':
-            n = int(a)
-        if o == '-r':
-            r = int(a)
+        if o == '-N':
+            N = int(a)
         if o == '-k':
             k = int(a)
-    if n == None or r == None or k == None:
-        print >> sys.stderr, "Missing arguments"
-        sys.exit(2) 
-    for m in range(n + 1):
-        if m > r / k:
-            break
-        print comb(n, m) * comb(r - 1 - m * k, n - 1)
-    
+    if N == None or k == None:
+        print >> sys.stderr, "Missing options"
+        sys.exit(2)
+    prob = rand_read_len_prob(N, k)
+    plt.plot(range(len(prob)), prob)
+    plt.grid(True)
+    plt.show()
+
 if __name__ == '__main__':
     main(sys.argv[1:])
-
