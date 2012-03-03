122de4559101ec2053279cd725177aa24f96d536
tmy1018.1@gmail.com <tmy1018.1@gmail.com@2d419d04-3b34-27e7-1591-4006bd207e75>
2012-03-03 09:07:08 +0000

    do it faster

--- trunk/trans-len/calc_L_N_from_c_n.py
+++ trunk/trans-len/calc_L_N_from_c_n.py
@@ -14,15 +14,39 @@
 #
 # You should have received a copy of the GNU General Public License
 
+"""
+compute estimators for all $(contig_len, contig_reads)$
+$0 \leq contig_len \leq c$, $1 \leq contig_reads \leq n$
+"""
+
 import getopt
 import sys
-from nzmath.combinatorial import stirling2
-from scipy import comb, factorial
+from scipy import comb
+from fractions import Fraction
+
+import help_1
+
+def get_cn_num(L, N, c, n, k):
+    sum1 = 0
+    if L - (2 * k + c + 1) >= 0:
+        if L - (2 * k + c + 1) == 0:
+            sum1 += 1
+        else:
+            sum1 += (((L - (2 * k + c + 1)) ** (N - n)) * (L - (2 * k + c)))
+    for i in range(k):
+        if L - (i + k + c + 1) >= 0:
+            if L - (i + k + c + 1) == 0:
+                sum1 += 2
+            else:
+                sum1 += (2 * (L - (i + k + c)) * ((L - (i + k + c + 1)) ** (N - n)))
+    if sum1 == 0:
+        return 0
+    return sum1 * comb(N, n, exact=True) * help_1.read_split_contig(n, c, k)
 
 def main(args):
-    c, n = None, None
+    c, n, k = None, None, None
     try:
-        opts, args = getopt.getopt(args, 'c:n:')
+        opts, args = getopt.getopt(args, 'c:n:k:')
     except getopt.GetoptError as err:
         print >> sys.stderr, str(err)
     for o, a in opts:
@@ -30,7 +54,9 @@ def main(args):
             c = int(a)
         if o == '-n':
             n = int(a)
-    if c == None or n == None:
+        if o == '-k':
+            k = int(a)
+    if c == None or n == None or k == None:
         print >> sys.stderr, "Missing options"
 
 if __name__ == '__main__':
