dec45de54982bd1cc8764ac5794c7b14a34b25c2
tmy1018.1@gmail.com <tmy1018.1@gmail.com@2d419d04-3b34-27e7-1591-4006bd207e75>
2012-03-05 16:24:51 +0000

    do many thiss b4 i sleep

--- uniform-test/get_reads_mapped_to_db_id_usearch_b6.py
+++ uniform-test/get_reads_mapped_to_db_id_usearch_b6.py
@@ -16,28 +16,37 @@
 
 import sys
 import getopt
+from Bio import SeqIO
 
 def main(args):
-    fmt, db, b6 = None, None, None, None
+    db, b6 = None, None
     try:
-        opts, reads_fins = getopt.getopt(args, 'f:d:b:')
+        opts, args = getopt.getopt(args, 'd:b:')
     except getopt.GetoptError as err:
         print >> sys.stderr, str(err)
         sys.exit(2)
     for o, a in opts:
-        if o == '-f':
-            # format of reads
-            fmt = a
         if o == '-d':
             # file containing all the db seqs, assumed to be in FASTA
             db = a
         if o == '-b':
             # usearch b6 files saying which reads mapped to db seqs
             b6 = a
-    if fmt == None or db == None or b6 == None or reads_fins == []:
+    if db == None or b6 == None:
         print >> sys.stderr, "missing arguments"
         sys.exit(2)
 
+    db_ids = set([])
+    for rec in SeqIO.parse(db, 'fasta'):
+        db_ids.add(rec.id)
+        
+    with open(b6,'r') as b6_fin:
+        for entry in b6_fin:
+            entry = entry.strip().split("\t")
+            contig_id = entry[1].split(" ")[0]
+            read_id = entry[0]
+            if contig_id in db_ids:
+                print read_id
 
 
 if __name__ == '__main__':
