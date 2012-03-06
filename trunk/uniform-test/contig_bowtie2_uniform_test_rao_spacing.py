18c81509ea2da42b456fcbf828546c674217f63c
tmy1018.1@gmail.com <tmy1018.1@gmail.com@2d419d04-3b34-27e7-1591-4006bd207e75>
2012-03-06 14:00:16 +0000

    jvoiroij

--- trunk/uniform-test/contig_bowtie2_uniform_test_rao_spacing.py
+++ trunk/uniform-test/contig_bowtie2_uniform_test_rao_spacing.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from HTSeq import SAM_Reader
 
 # Copyright (C) 2012 Tianyang Li
 #
@@ -27,15 +26,19 @@ from rpy2.robjects.packages import importr
 from Bio import SeqIO
 from HTSeq import SAM_Reader
 import json
+from scipy.constants import pi
 
 def main(args):
-    bowtie2, read_len, contigs, fout = None, None, None, None
+    bowtie2, read_len, contigs, fout, alpha = None, None, None, None, None
     try:
-        opts, args = getopt.getopt(args, 'b:c:l:o:')
+        opts, args = getopt.getopt(args, 'b:c:l:o:a:')
     except getopt.GetoptError as err:
         print >> sys.stderr(err)
         sys.exit(2)
     for opt, arg in opts:
+        if opt == '-a':
+            # alpha used in test
+            alpha = float(arg)
         if opt == '-b':
             # bowtie2 SAM file
             bowtie2 = arg
@@ -45,9 +48,9 @@ def main(args):
             # assembled contigs (FASTA)
             contigs = arg
         if opt == '-o':
-            # output file
+            # output file containing ids that do not pass the test
             fout = arg
-    if bowtie2 == None or read_len == None or contigs == None or fout == None:
+    if bowtie2 == None or read_len == None or contigs == None or fout == None or alpha == None:
         print >> sys.stderr, "Missing options"
         sys.exit(2)
     
@@ -60,7 +63,25 @@ def main(args):
             contigs_len[align.iv.chrom][1].append(align.iv.start)
 
     with open(fout, 'w') as res:
+        circular = importr('circular')
+        rst = circular.rao_spacing_test
+        circ = circular.circular
         
+        for contig_id, contig_entry in contigs_len.items(): 
+            if contig_entry[0] >= read_len:
+                eff_len = contig_entry[0] - read_len + 1
+                pos_in_rad = []
+                for start_pos in contig_entry[1]:
+                    if start_pos < eff_len:
+                        pos_in_rad.append(2 * pi * float(start_pos) / float(eff_len))
+                if len(pos_in_rad) > 3:
+                    rst_res = rst(circ(robj.FloatVector(pos_in_rad)), alpha=alpha)
+                    rst_res = str(rst_res)
+                    print rst_res
+                    rst_res = rst_res.split("\n")[5]
+                    if "Reject" in rst_res:
+                        res.write("%s\n" % contig_id)
+                
     
 if __name__ == '__main__':
     main(sys.argv[1:])
